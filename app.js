d6333db041d6f6fe3f529295772d4c83e696e87e
Vlad <vladislafus@gmail.com>
2015-09-25 14:14:24 +0300

    Add list items

--- app.js
+++ app.js
@@ -1,26 +1,55 @@
 var TODO = React.createClass({
+  getInitialState: function(){
+    return {
+			items: [
+				{id:1, title: "One"},
+				{id:2, title: "Two"},
+				{id:3, title: "Three"}
+			]
+		};
+  },
   render: function(){
     return(
+      <div>
       <div className="panel panel-default">
         <div className="panel-body">
-          <InputForm />
+        <InputForm />
         </div>
       </div>
+      <ul className="list-group">
+      {
+        this.state.items.map(function(item){
+          return  <List title={item.title} />;
+        })
+      }
+      </ul>
+      </div>
+    );
+  }
+});
+
+var List = React.createClass({
+  render: function(){
+    return(
+        <li className="list-group-item">{this.props.title}</li>
     );
   }
 });
 
 
 var InputForm = React.createClass({
-  handleSubmit: function(){},
+  handleSubmit: function(e){
+    e.preventDefault();
+    console.log('Add todo');
+  },
   render: function(){
     return(
-      <form className="form-inline" role="form">
+      <form className="form-inline" onSubmit={this.handleSubmit} role="form">
       <input type="text"
-            name="item"
-            className="form-control"
-            />
-      <button className="btn btn-primary">Add </button>
+      name="item"
+      className="form-control"
+      />
+      <button className="btn btn-primary">Add</button>
       </form>
     );
   }
